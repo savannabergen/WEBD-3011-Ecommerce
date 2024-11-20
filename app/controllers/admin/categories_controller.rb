537a0a1d7629f974a7a25110ff36c608ef222637
Savanna Bergen <savanna.bergen@gmail.com>
2024-11-19 20:15:20 -0600

    rubocop errors fixed

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -19,52 +19,52 @@ class Admin::CategoriesController < AdminController
   def edit
   end
 
-  # POST /admin/categories or /admin/categories.json
-  def create
-    @admin_category = Category.new(admin_category_params)
+ # POST /admin/categories or /admin/categories.json
+ def create
+  @admin_category = Category.new(admin_category_params)
 
-    respond_to do |format|
-      if @admin_category.save
-        format.html { redirect_to admin_category_path(@admin_category), notice: "Category was successfully created." }
-        format.json { render :show, status: :created, location: @admin_category }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @admin_category.errors, status: :unprocessable_entity }
-      end
+  respond_to do |format|
+    if @admin_category.save
+      format.html { redirect_to admin_category_url(@admin_category), notice: "Category was successfully created." }
+      format.json { render :show, status: :created, location: @admin_category }
+    else
+      format.html { render :new, status: :unprocessable_entity }
+      format.json { render json: @admin_category.errors, status: :unprocessable_entity }
     end
   end
+end
 
-  # PATCH/PUT /admin/categories/1 or /admin/categories/1.json
-  def update
-    respond_to do |format|
-      if @admin_category.update(admin_category_params)
-        format.html { redirect_to admin_category_path(@admin_category), notice: "Category was successfully updated." }
-        format.json { render :show, status: :ok, location: @admin_category }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @admin_category.errors, status: :unprocessable_entity }
-      end
+# PATCH/PUT /admin/categories/1 or /admin/categories/1.json
+def update
+  respond_to do |format|
+    if @admin_category.update(admin_category_params)
+      format.html { redirect_to admin_category_url(@admin_category), notice: "Category was successfully updated." }
+      format.json { render :show, status: :ok, location: @admin_category }
+    else
+      format.html { render :edit, status: :unprocessable_entity }
+      format.json { render json: @admin_category.errors, status: :unprocessable_entity }
     end
   end
+end
 
-  # DELETE /admin/categories/1 or /admin/categories/1.json
-  def destroy
-    @admin_category.destroy!
+# DELETE /admin/categories/1 or /admin/categories/1.json
+def destroy
+  @admin_category.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to admin_categories_path, status: :see_other, notice: "Category was successfully destroyed." }
-      format.json { head :no_content }
-    end
+  respond_to do |format|
+    format.html { redirect_to admin_categories_url, notice: "Category was successfully destroyed." }
+    format.json { head :no_content }
+  end
+end
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_admin_category
+    @admin_category = Category.find(params[:id])
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_admin_category
-      @admin_category = Category.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def admin_category_params
-      params.require(:admin_category).permit(:name, :description)
-    end
+  # Only allow a list of trusted parameters through.
+  def admin_category_params
+    params.require(:category).permit(:name, :description, :image)
+  end
 end
