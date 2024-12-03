3c6c11aa7a7e57d59fa78f24a401abe72a0bab40
Savanna Bergen <savanna.bergen@gmail.com>
2024-12-03 09:39:59 -0600

    stripe gem added, checkouts_controller added

---
+++ app/controllers/checkouts_controller.rb
@@ -0,0 +1,4 @@
+class CheckoutsController < ApplicationController
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -29,4 +29,5 @@ Rails.application.routes.draw do
 
   get "admin" => "admin#index"
   get "cart" => "carts#show"
+  post "checkout" => "checkouts#create"
 end
