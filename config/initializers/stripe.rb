d98a0dc3e8b377c450486351857a0b2012571498
Savanna Bergen <savanna.bergen@gmail.com>
2024-12-04 13:06:40 -0600

    removed stripe.rb

--- config/initializers/stripe.rb
+++
@@ -1,5 +0,0 @@
-Rails.configuration.stripe = {
-  publishable_key: ENV["pk_test_51QRyDdDzoMWjx7vqDlNP2hWneXRXBhh0QofExKgkqkLIDQCq1uTjjvpNtpy9pPfPnPT9YKfavQElsbAVS4WAvusN00gqle6MDx"],
-  secret_key: ENV["sk_test_51QRyDdDzoMWjx7vqsHBuWKQY1r8Sqm2sEufUJjWfX7meD3TUkpyAVShn5Wr6CYxS0NgVgpPrtYuTMUxzh2gsS9Kb00qoyuowXH"]
-}
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
