9ab235a61eaf8b7741cc03e9615b6d338d237536
Savanna Bergen <savanna.bergen@gmail.com>
2024-12-04 13:44:43 -0600

    stripe logic completed, ready to deploy

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,9 +1,11 @@
 class CheckoutsController < ApplicationController
   def create
+    stripe_secret_key = Rails.application.credentials.dig(:production, :stripe_api_secret)
+    Stripe.api_key = stripe_secret_key
     cart = params[:cart]
     line_items = cart.map do |item|
       product = Product.find(item["id"])
-      product_stock = product.stocks.find { |ps| ps.size == item["size"] }
+      product_stock = product.stocks.find{ |ps| ps.size == item["size"] }
 
       if product_stock.amount < item["quantity"].to_i
         render json: { error: "Not enough stock for #{product.name} in size #{item["size"]}. Only #{product_stock.amount} left." }, status: 400
@@ -17,7 +19,7 @@ class CheckoutsController < ApplicationController
             name: item["name"],
             metadata: { product_id: product.id, size: item["size"], product_stock_id: product_stock.id }
           },
-          currency: "cad",
+          currency: "usd",
           unit_amount: item["price"].to_i
         }
       }
@@ -31,7 +33,7 @@ class CheckoutsController < ApplicationController
       success_url: "http://localhost:3000/success",
       cancel_url: "http://localhost:3000/cancel",
       shipping_address_collection: {
-        allowed_countries: [ "CA" ]
+        allowed_countries: ['US', 'CA']
       }
     )
 
