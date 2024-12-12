12c101ca6bfc70009b3d497a54bd6a5e405e3e47
Savanna Bergen <savanna.bergen@gmail.com>
2024-12-12 15:49:48 -0600

    categories_controller updated with params to search for min/max price

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,62 @@
 require 'faker'
 
-10.times do
-  Category.create(name: Faker::Commerce.department, description: Faker::Lorem.paragraph)
+Admin.create!(
+  email: 'admin@example.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+categories = [
+  { name: 'T-Shirts', description: 'Casual and comfortable tees' },
+  { name: 'Hoodies', description: 'Cozy and stylish hoodies' },
+  { name: 'Jeans', description: 'Classic and trendy jeans' },
+  { name: 'Shoes', description: 'Sneakers and boots for every occasion' }
+]
+
+categories_with_ids = Category.create!(categories)
+
+100.times do |i|
+  product = {
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph,
+    price: Faker::Number.within(range: 1000..50000),
+    active: true,
+    category_id: Category.pluck(:id).sample
+  }
+
+  begin
+    Product.create!(product)
+  rescue ActiveRecord::RecordInvalid => e
+    puts "Error creating product: #{e.message}"
+  end
 end
 
-10.times do
-  Product.create(
-  name: Faker::Commerce.product_name,
-  description: Faker::Lorem.paragraph,
-  price: Faker::Commerce.price,
-  category_id: Category.pluck(:id).sample,
-  active: true
-  )
+Product.all.each do |product|
+  sizes = [ 'S', 'M', 'L', 'XL' ]
+  sizes.each do |size|
+  stock = {
+  size: size,
+  amount: Faker::Number.within(range: 1..100),
+  product_id: Product.pluck(:id).sample
+  }
+  Stock.create!(stock)
+  end
 end
 
-10.times do
-  Order.create(
-  customer_email: Faker::Internet.email,
-  fulfilled: Faker::Boolean.boolean,
-  total: Faker::Commerce.price,
-  address: Faker::Address.full_address
+50.times do
+  order = Order.new(
+    customer_email: Faker::Internet.email,
+    fulfilled: false,
+    total: Faker::Number.within(range: 10..100)
   )
+  order.save!
+end
+
+50.times do
+  order = Order.new(
+    customer_email: Faker::Internet.email,
+    fulfilled: true,
+    total: Faker::Number.within(range: 10..100),
+  )
+  order.save!
 end
